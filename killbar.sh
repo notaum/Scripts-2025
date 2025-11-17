486e5e6f267b67afc9750d333569c43b8802a1a9
Aum <dontmailaum@tuta.io>
2025-11-17 13:06:03 +0530

    Moved files to their relevant config folders

--- killbar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-killall waybar
-notify-send -u critical -t 1500 "Killed Waybar"
--- waybar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-killall waybar
-waybar &
-notify-send -u critical -t 1500 "Waybar Reloaded"
